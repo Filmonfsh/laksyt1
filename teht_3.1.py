8413d38a2f3f7ffb5656cc36efb34696da368199
Filmonfsh <filmonfsh1234@gmail.com>
2026-08-18 15:00:20 +0300

    lisättiin kala-thtava

---
+++ teht_3.1.py
@@ -0,0 +1,9 @@
+pituus = float(input("kuhan pitus :"))
+
+if pituus < 37:
+
+    alamittaisuus = 37 - pituus
+    print(f"Kalasi on {alamittaisuus} on liian lyhyt!")
+
+else:
+    print("Kalasi voit syödä!")
