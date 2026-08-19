9c3b41a1cb58c4431cddea04d30deee14b2c1589
Filmonfsh <filmonfsh1234@gmail.com>
2026-08-19 10:55:55 +0300

    Lisätiin kokonaisluvut

---
+++ teht_2.4.py
@@ -0,0 +1,10 @@
+#Annetaan kolma kokonaista luvut
+
+ensimmaine = int(input("Anna ensimmaine luku: "))
+toinen = int(input("Anna toinen luku: "))
+kolmas = int(input("Anna kolmas luku: "))
+
+#Tulostetaan keskiarvo tulos
+yhtensumma = ensimmaine + toinen + kolmas
+keskiarvo = yhtensumma / 3
+print("keskiarvo on ", keskiarvo)
